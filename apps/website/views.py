a95bb05758dc7ca9fe62d42d25d718c28be8d3c5
jaspesoft <jaspesoft@gmail.com>
2020-04-28 17:57:37 -0300

    enviar banners para el front

--- apps/website/views.py
+++ apps/website/views.py
@@ -3,12 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.generic import TemplateView
 
-from apps.administrador.models import Categorias
+from apps.administrador.models import Categorias, Banners
 
 
 def getCategoriasPrincipal():
     return Categorias.objects.filter(
         principal=True)
+
+def getBanners():
+    return Banners.objects.filter(activo=True)
 
 class Home(TemplateView):
     template_name = 'website/home.html'
@@ -16,5 +19,6 @@ class Home(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(Home, self).get_context_data(**kwargs)
         context['cat_padres'] = getCategoriasPrincipal()
+        context['banners'] = getBanners()
 
         return context
